4183e1a6347098bb6acec0177894b6b8a30533db
phparreira <33208171+phparreira@users.noreply.github.com>
2018-07-17 20:33:23 -0300

    Update Projeto1.py

--- Projeto1.py
+++ Projeto1.py
@@ -1,72 +1,166 @@
+from os import listdir
+import os
 import librosa
 import sklearn
+import sklearn
 import numpy as np
-from os import listdir
-from sklearn.ensemble import GradientBoostingClassifier
+import librosa.display
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from math import exp
 from sklearn.cluster import KMeans  
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestClassifier
+nLetras   = 4
 n_mfcc    = 12
-rotulos   = []
+
+def rotuloGrupo(rotulo):
+    if ('a'  in rotulo) or ('b' in rotulo)  or ('d' in rotulo):
+        return  'g1'
+    elif ('m' in rotulo) or ('n' in rotulo) or ('h' in rotulo):
+        return  'g2'
+    elif ('x' in rotulo) or ('6' in rotulo) or ('7' in rotulo) or ('c' in rotulo):
+        return  'g3'
+
+def SegmentarAudio(audio,fs,n,tamanho,tempo):
+    #y_percussive = librosa.effects.percussive(audio)
+    #D = np.abs(librosa.stft(y_percussive))**2
+    D = np.abs(librosa.stft(audio))**2
+    S = librosa.feature.melspectrogram(S=D)
+    db = librosa.amplitude_to_db(S)
+    bounds = librosa.segment.agglomerative(db, 40)
+    bound_times = librosa.frames_to_time(bounds, sr=fs)
+    bound_times = np.vstack((bound_times[2:], [0]*len(bound_times[2:]))).T
+    kmeans = KMeans(n_clusters=n)  
+    kmeans.fit(bound_times[2:])  
+    func1 = lambda x: int(tamanho*(x/tempo))
+    centros =  np.array([func1(xi) for xi in kmeans.cluster_centers_[:,0]])
+    centros =  np.append(centros,tamanho)
+    centros.sort()
+    ultimo  = 0
+    retorno = []
+    for i,j in zip(centros,centros[1:]):
+        sl = int((i+j)/2)
+        retorno.append(audio[ultimo:sl])
+        ultimo = sl
+    return np.array(retorno)
+
+
+
+# Treina o o modelo
+os.chdir('base_treinamento_I')
+
+
+scaler = sklearn.preprocessing.StandardScaler()
 atributos = []
-rotulo    = ""
-
-#Extrai todos as matrizes dos MFCC dos aúdios percursivos de treinamento 
+rotulos   = []
+nItens = 0
 for arquivo in listdir():
-    if 'wav' in arquivo:
+    if 'wav' in arquivo: 
         audio, fs = librosa.load(arquivo,None)
-        y_harmonic, y_percussive = librosa.effects.hpss(audio)
-        mfcc   =  librosa.feature.mfcc(y_percussive, sr=fs, n_mfcc=n_mfcc).T
-        scaler = sklearn.preprocessing.StandardScaler()
-        mfcc_scaled = scaler.fit_transform(mfcc)
-        atributos.append(mfcc_scaled)
-        if ('a'  in [arquivo[0:1]]) or ('b' in [arquivo[0:1]])  or ('d' in [arquivo[0:1]]):
-            rotulo = 'g1'
-        elif ('m' in [arquivo[0:1]]) or ('n' in [arquivo[0:1]]) or ('h' in [arquivo[0:1]]):
-            rotulo = 'g2'
-        elif ('x' in [arquivo[0:1]]) or ('6' in [arquivo[0:1]]) or ('7' in [arquivo[0:1]]) or ('c' in [arquivo[0:1]]):
-            rotulo = 'g3'
-        rotulos=rotulos+[rotulo]*len(mfcc_scaled)
+        tamanho = audio.shape[0]
+        tempo   = audio.shape[0]/fs
+        retorno = SegmentarAudio(audio,fs,nLetras,tamanho,tempo)
+        nItens = nItens + 1
+        print(nItens)
+        for i in range(0,nLetras):
+             S       = np.abs(librosa.stft(retorno[i,]))
+             y_harm, y_per = librosa.effects.hpss(retorno[i,])
+             #p2      = librosa.feature.poly_features(S=S, order=2)
+             #atr1    = librosa.feature.chroma_stft(y=retorno[i,], sr=fs).T
+             mfcc1   =  librosa.feature.mfcc(y_harm, sr=fs, n_mfcc=n_mfcc).T
+             #atr1    = scaler.fit_transform(mfcc1)
+             mfcc2   =  librosa.feature.mfcc(y_per, sr=fs, n_mfcc=n_mfcc).T
+             #atr2    = scaler.fit_transform(mfcc2)
+             chrm1   = librosa.feature.chroma_stft(y=retorno[i,], sr=fs).T
+             t = np.concatenate([mfcc1,chrm1])
+             atr1    = scaler.fit_transform(t)
+             #print(atr1.shape)
+             atributos.append(atr1)
+             #rotulos=rotulos+[(arquivo[i])]*len(atr1)
+             rotulos=rotulos+[rotuloGrupo(arquivo[i])]*len(atr1)
 
-#Constroi o modelo
 atributos = np.vstack((atributos))
 rotulos   = np.array(rotulos)
-model = sklearn.ensemble.GradientBoostingClassifier() #OVA(one vs all)
+#Naive Bayses
+#gnb = GaussianNB()
+#model = sklearn.ensemble.GradientBoostingClassifier() #OVA(one vs all)
+model = RandomForestClassifier(n_estimators=10)
 model.fit(atributos, rotulos)
 
 
-# Abre o arquivo de teste
-audio, fs = librosa.load('nbdd.c',None)
-tamanho = audio.shape[0]
-tempo   = audio.shape[0]/fs
-#Retira as características para a segmentação
-y_harmonic, y_percussive = librosa.effects.hpss(audio)
-D = np.abs(librosa.stft(y_percussive))**2
-S = librosa.feature.melspectrogram(S=D)
-db = librosa.amplitude_to_db(S)
 
-bounds = librosa.segment.agglomerative(db, 40)
-bound_times = librosa.frames_to_time(bounds, sr=fs)
-bound_times = np.vstack((bound_times[2:], [0]*len(bound_times[2:]))).T
+#Testa
+os.chdir('base_validacao_I')
 
 
-kmeans = KMeans(n_clusters=4)  
-kmeans.fit(bound_times[2:])  
+scaler = sklearn.preprocessing.StandardScaler()
+atributos = []
+rotulos   = []
+nAudios   = 0
+nAcertos  = 0
+nErros    = 0 
+nItens    = 0
+pred      = []
+vdd       = []
+for arquivo in listdir():
+    if 'wav' in arquivo: 
+        print("Novo")
+        print(nErros)
+        print(nAcertos)
+        audio, fs = librosa.load(arquivo,None)
+        tamanho = audio.shape[0]
+        tempo   = audio.shape[0]/fs
+        retorno = SegmentarAudio(audio,fs,nLetras,tamanho,tempo)
+        nAudios = nAudios + 4
+        nItens = nItens + 1
+        print(nItens)
+        for i in range(0,nLetras):
+             S       = np.abs(librosa.stft(retorno[i,]))
+             y_harm, y_per = librosa.effects.hpss(retorno[i,])
+             #p2      = librosa.feature.poly_features(S=S, order=2)
+             #mfcc   =  librosa.feature.mfcc(retorno[i,], sr=fs, n_mfcc=n_mfcc).T
+             #atr1    = librosa.feature.chroma_stft(y=retorno[i,], sr=fs).T
+             mfcc1   =  librosa.feature.mfcc(y_harm, sr=fs, n_mfcc=n_mfcc).T
+             #atr1    = scaler.fit_transform(mfcc1)
+             mfcc2   =  librosa.feature.mfcc(y_per, sr=fs, n_mfcc=n_mfcc).T
+             #atr2    = scaler.fit_transform(mfcc2)
+             chrm1   = librosa.feature.chroma_stft(y=retorno[i,], sr=fs).T
+             t = np.concatenate([mfcc1,chrm1])
+             atr1    = scaler.fit_transform(t)
 
-func1 = lambda x: int(tamanho*(x/tempo))
-centros =  np.array([func1(xi) for xi in kmeans.cluster_centers_[:,0]])
-centros =  np.append(centros,tamanho)
-centros.sort()
+             pred    = model.predict(atr1)   
+             (values,counts) = np.unique(pred,return_counts=True)
+             ind = np.argmax(counts)
+             #vdd = np.append(vdd,(arquivo[i],values[ind]))
+             if values[ind] == rotuloGrupo(arquivo[i]):
+             #if values[ind] == arquivo[i]:
+                nAcertos = nAcertos + 1
+                print("ACERTOU")
+             else:
+                nErros = nErros + 1
+                print("ERROU")
+                print(arquivo[i])
+                print(values[ind])
 
-ultimo = 0
-for i,j in zip(centros,centros[1:]):
-    sl = int((i+j)/2)
-    dado = audio[ultimo:sl]
-    y_harmonic, y_percussive = librosa.effects.hpss(dado)
-    mfcc   =  librosa.feature.mfcc(y_percussive, sr=fs, n_mfcc=n_mfcc).T
-    scaler = sklearn.preprocessing.StandardScaler()
-    mfcc_scaled = scaler.fit_transform(mfcc)
-    preditor = model.predict(mfcc_scaled)
-    print(([(preditor == c).sum() for c in ('g1','g2','g3')]))
-    librosa.output.write_wav('%s.wav' %i, dado,  fs)
-    ultimo = sl
+print(nAudios)
+print(nAcertos)
+print(nErros)
 
+_ = joblib.dump(model, 'modelo_FT_Grupo.bb', compress=9)
+
+#BASELINE
+#---------------------------------------------------------------------------------------------------------------------
+#TESTE1 -> NB + CHROMA_STFT        12.8% [90/702]
+#TESTE2 -> NB + MFCC               12.8% [90/702]
+#TESTE4 ->GradientBoostingClassifier + MFCC        45.96% [364/792]
+#TESTE5 ->GradientBoostingClassifier + CHROMA_STFT 14.39% [114/792]
+#TESTE7 ->RandomForestClassifier     + MFCC        56.18% [445/792] 
+#TESTE7 ->RandomForestClassifier     + MFCC[h/p]   57.19% [453/792] 
+#---------------------------------------------------------------------------------------------------------------------
+#TESTE6 ->GradientBoostingClassifier + MFCC 69.82% [553/792] [GRUPOS]
+#TESTE6 ->RandomForestClassifier     + MFCC 77.39% [613/792] [GRUPOS]
+#TESTE6 ->RandomForestClassifier     + MFCC[h/p] 79.92% [633/792] [GRUPOS]
